df64232f289a1f69e4d14a8e55c17a6fa34e99c7
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-12-08 14:19:04 +0300

    Initial commit

---
+++ Monopoly/Monopoly.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class Monopoly
+    {
+        public List<Tuple<string, int>> players = new List<Tuple<string, int>>();
+        public List<Tuple<string, Monopoly.Type, int, bool>> fields = new List<Tuple<string, Type, int, bool>>();
+        public Monopoly(string[] p, int v)
+        {
+            for (int i = 0; i < v; i++)
+            {
+                players.Add(new Tuple<string,int>(p[i], 6000));     
+            }
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("Ford", Monopoly.Type.AUTO, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("MCDonald", Monopoly.Type.FOOD, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("Lamoda", Monopoly.Type.CLOTHER, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("Air Baltic", Monopoly.Type.TRAVEL, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("Nordavia", Monopoly.Type.TRAVEL, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("Prison", Monopoly.Type.PRISON, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("MCDonald", Monopoly.Type.FOOD, 0, false));
+            fields.Add(new Tuple<string, Monopoly.Type, int, bool>("TESLA", Monopoly.Type.AUTO, 0, false));
+        }
+
+        internal List<Tuple<string, int>> GetPlayersList()
+        {
+            return players;
+        }
+
+        internal enum Type
+        {
+            AUTO,
+            FOOD,
+            CLOTHER,
+            TRAVEL,
+            PRISON,
+            BANK
+        }
+
+        internal List<Tuple<string, Monopoly.Type, int, bool>> GetFieldsList()
+        {
+            return fields;
+        }
+
+        internal Tuple<string, Type, int, bool> GetFieldByName(string v)
+        {
+            return (from p in fields where p.Item1 == v select p).FirstOrDefault();
+        }
+
+        internal bool Buy(int v, Tuple<string, Type, int, bool> k)
+        {
+            var x = GetPlayerInfo(v);
+            int cash = 0;
+            switch(k.Item2)
+            {
+                case Type.AUTO:
+                    if (k.Item3 != 0)
+                        return false;
+                    cash = x.Item2 - 500;
+                    players[v - 1] = new Tuple<string, int>(x.Item1, cash);
+                    break;
+                case Type.FOOD:
+                    if (k.Item3 != 0)
+                        return false;
+                    cash = x.Item2 - 250;
+                    players[v - 1] = new Tuple<string, int>(x.Item1, cash);
+                    break;
+                case Type.TRAVEL:
+                    if (k.Item3 != 0)
+                        return false;
+                    cash = x.Item2 - 700;
+                    players[v - 1] = new Tuple<string, int>(x.Item1, cash);
+                    break;
+                case Type.CLOTHER:
+                    if (k.Item3 != 0)
+                        return false;
+                    cash = x.Item2 - 100;
+                    players[v - 1] = new Tuple<string, int>(x.Item1, cash);
+                    break;
+                default:
+                    return false;
+            }
+            int i = players.Select((item, index) => new { name = item.Item1, index = index })
+                .Where(n => n.name == x.Item1)
+                .Select(p => p.index).FirstOrDefault();
+            fields[i] = new Tuple<string, Type, int, bool>(k.Item1, k.Item2, v, k.Item4);
+             return true;
+        }
+
+        internal Tuple<string, int> GetPlayerInfo(int v)
+        {
+            return players[v - 1];
+        }
+
+        internal bool Renta(int v, Tuple<string, Type, int, bool> k)
+        {
+            var z = GetPlayerInfo(v);
+            Tuple<string, int> o = null;
+            switch(k.Item2)
+            {
+                case Type.AUTO:
+                    if (k.Item3 == 0)
+                        return false;
+                    o =  GetPlayerInfo(k.Item3);
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 250);
+                    o = new Tuple<string, int>(o.Item1,o.Item2 + 250);
+                    break;
+                case Type.FOOD:
+                    if (k.Item3 == 0)
+                        return false;
+                    o = GetPlayerInfo(k.Item3);
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 250);
+                    o = new Tuple<string, int>(o.Item1, o.Item2 + 250);
+
+                    break;
+                case Type.TRAVEL:
+                    if (k.Item3 == 0)
+                        return false;
+                    o = GetPlayerInfo(k.Item3);
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 300);
+                    o = new Tuple<string, int>(o.Item1, o.Item2 + 300);
+                    break;
+                case Type.CLOTHER:
+                    if (k.Item3 == 0)
+                        return false;
+                    o = GetPlayerInfo(k.Item3);
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 100);
+                    o = new Tuple<string, int>(o.Item1, o.Item2 + 1000);
+
+                    break;
+                case Type.PRISON:
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 1000);
+                    break;
+                case Type.BANK:
+                    z = new Tuple<string, int>(z.Item1, z.Item2 - 700);
+                    break;
+                default:
+                    return false;
+            }
+            players[v - 1] = z;
+            if(o != null)
+                players[k.Item3 - 1] = o;
+            return true;
+        }
+    }
+}
---
+++ Monopoly/TestClass.cs
@@ -0,0 +1,75 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Monopoly
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void GetPlayersListReturnCorrectList()
+        {
+            string[] players = new string[]{ "Peter","Ekaterina","Alexander" };
+            Tuple<string, int>[] expectedPlayers = new Tuple<string, int>[]
+            {
+                new Tuple<string, int>("Peter",6000),
+                new Tuple<string, int>("Ekaterina",6000),
+                new Tuple<string, int>("Alexander",6000)
+            };
+            Monopoly monopoly = new Monopoly(players,3);
+            Tuple<string, int>[] actualPlayers = monopoly.GetPlayersList().ToArray();
+
+            Assert.AreEqual(expectedPlayers, actualPlayers);
+        }
+        [Test]
+        public void GetFieldsListReturnCorrectList()
+        {
+            Tuple<string, Monopoly.Type, int, bool>[] expectedCompanies = 
+                new Tuple<string, Monopoly.Type, int, bool>[]{
+                new Tuple<string,Monopoly.Type,int,bool>("Ford",Monopoly.Type.AUTO,0,false),
+                new Tuple<string,Monopoly.Type,int,bool>("MCDonald", Monopoly.Type.FOOD, 0, false),
+                new Tuple<string,Monopoly.Type,int,bool>("Lamoda", Monopoly.Type.CLOTHER, 0, false),
+                new Tuple<string, Monopoly.Type, int, bool>("Air Baltic",Monopoly.Type.TRAVEL,0,false),
+                new Tuple<string, Monopoly.Type, int, bool>("Nordavia",Monopoly.Type.TRAVEL,0,false),
+                new Tuple<string, Monopoly.Type, int, bool>("Prison",Monopoly.Type.PRISON,0,false),
+                new Tuple<string, Monopoly.Type, int, bool>("MCDonald",Monopoly.Type.FOOD,0,false),
+                new Tuple<string, Monopoly.Type, int, bool>("TESLA",Monopoly.Type.AUTO,0,false)
+            };
+            string[] players = new string[] { "Peter", "Ekaterina", "Alexander" };
+            Monopoly monopoly = new Monopoly(players, 3);
+            Tuple<string, Monopoly.Type, int, bool>[] actualCompanies = monopoly.GetFieldsList().ToArray();
+            Assert.AreEqual(expectedCompanies, actualCompanies);
+        }
+        [Test]
+        public void PlayerBuyNoOwnedCompanies()
+        {
+            string[] players = new string[] { "Peter", "Ekaterina", "Alexander" };
+            Monopoly monopoly = new Monopoly(players, 3);
+            Tuple<string, Monopoly.Type, int, bool> x = monopoly.GetFieldByName("Ford");
+            monopoly.Buy(1, x);
+            Tuple<string,int> actualPlayer = monopoly.GetPlayerInfo(1);
+            Tuple<string, int> expectedPlayer = new Tuple<string, int>("Peter", 5500);
+            Assert.AreEqual(expectedPlayer, actualPlayer);
+            Tuple<string, Monopoly.Type, int, bool> actualField = monopoly.GetFieldByName("Ford");
+            Assert.AreEqual(1, actualField.Item3);
+        }
+        [Test]
+        public void RentaShouldBeCorrectTransferMoney()
+        {
+            string[] players = new string[] { "Peter", "Ekaterina", "Alexander" };
+            Monopoly monopoly = new Monopoly(players, 3);
+            Tuple<string, Monopoly.Type, int, bool>  x = monopoly.GetFieldByName("Ford");
+            monopoly.Buy(1, x);
+            x = monopoly.GetFieldByName("Ford");
+            monopoly.Renta(2, x);
+            Tuple<string, int> player1 = monopoly.GetPlayerInfo(1);
+            Assert.AreEqual(5750, player1.Item2);
+            Tuple<string, int> player2 = monopoly.GetPlayerInfo(2);
+            Assert.AreEqual(5750, player2.Item2);
+        }
+    }
+}
